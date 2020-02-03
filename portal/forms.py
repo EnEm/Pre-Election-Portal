d426aff34445253aeed910f39a33c65b9036a165
Bhasker Goel <bhasker2000goel@gmail.com>
2020-02-03 19:36:27 +0530

    some changes

--- portal/forms.py
+++ portal/forms.py
@@ -1,9 +1,30 @@
 from django import forms
-
-from .models import Question
+from django_summernote.widgets import SummernoteWidget
+from .models import Question, Candidate
+from . import choices
 
 
 class AskForm(forms.ModelForm):
+    position = forms.ChoiceField(choices=choices.FORM_POSITION_CHOICES, initial=choices.SELECT)
+
     class Meta:
         model = Question
-        fields = ('question', 'asked_to')
+        fields = ('question', 'position', 'asked_to')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['asked_to'].queryset = Question.objects.none()
+
+        if 'position' in self.data:
+            try:
+                position = self.data.get('position')
+                self.fields['asked_to'].queryset = Candidate.objects.filter(position=position)
+
+            except(TypeError, ValueError):
+                print(TypeError)
+                print("---------")
+                print(ValueError)
+
+
+class AnswerForm(forms.Form):
+    answer = forms.CharField(widget=SummernoteWidget)
