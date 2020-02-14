96511790d4ed2cd6cc71bc7e708639ba5d6b3fda
unknown <hlaordsuh@users.noreply.github.com>
2020-02-14 22:11:49 +0530

    some changes

--- stats/views.py
+++ stats/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from portal.models import Hostel
 
+def chart(request):
+    
+    dataSource = {}
+    dataSource['data'] = []
+    for key in Hostel.objects.all():
+      data = {}
+      data['y'] = 100*key.no_of_votes/key.total_residents
+      data['label'] = key.name
+      dataSource['data'].append(data)
 
-def bchart(request):
-    context={
-        'hostels' : Hostel.objects.all()
-    }
-    return render(request,'stats/111.html',context)
+    dicti = {'output': dataSource['data']}
+    return render(request,'stats/111.html',context = dicti )
